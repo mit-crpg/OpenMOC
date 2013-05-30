17786233b73e866b18ed8677135cdf4ea6f44f07
wbinventor <wbinventor@gmail.com>
2013-05-30 15:03:36 -0500

    Removed gcc and icpc folders

--- openmoc/gcc/double/__init__.py
+++
@@ -1,7 +0,0 @@
-import openmoc
-import _openmoc_gcc_double
-from openmoc_gcc_double import *
-
-setLogLevel(str(openmoc.getLogLevel()))
-setOutputDirectory(openmoc.getOutputDirectory())
-setLogfileName(openmoc.getLogfileName())
--- openmoc/icpc/double/__init__.py
+++
@@ -1,7 +0,0 @@
-import openmoc
-import _openmoc_icpc_double
-from openmoc_icpc_double import *
-
-setLogLevel(str(openmoc.getLogLevel()))
-setOutputDirectory(openmoc.getOutputDirectory())
-setLogfileName(openmoc.getLogfileName())
