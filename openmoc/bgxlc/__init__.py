e30fc52ec19d4884f4a2ef668e5e1bc8e9254660
Will Boyd <wbinventor@gmail.com>
2013-08-13 21:05:09 -0400

    Removed extraneous files from git

--- openmoc/bgxlc/__init__.py
+++
@@ -1,9 +0,0 @@
-import openmoc
-import _openmoc_bgxlc_double
-from openmoc_bgxlc_double import *
-
-setLogLevel(str(openmoc.getLogLevel()))
-setOutputDirectory(openmoc.getOutputDirectory())
-setLogfileName(openmoc.getLogfileName())
-
-Timer = openmoc.Timer
