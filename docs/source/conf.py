dffa2f87f110e60da207a94fa695566b42e19fdf
Paul Romano <paul.k.romano@gmail.com>
2014-01-15 16:54:19 -0500

    Change year on copyright to 2014.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenMOC'
-copyright = u'2012-2013, Massachusetts Institute of Technology'
+copyright = u'2012-2014, Massachusetts Institute of Technology'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
