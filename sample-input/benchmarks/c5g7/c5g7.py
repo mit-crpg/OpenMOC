3b8871b43dd6d65c9ae1cc8fbd999afbff48b889
Will Boyd <wbinventor@gmail.com>
2014-01-25 00:26:07 -0500

    Fixed Lattice, Cell, Universe and Material printString() methods to not segmentation fault

--- sample-input/benchmarks/c5g7/c5g7.py
+++ sample-input/benchmarks/c5g7/c5g7.py
@@ -201,6 +201,8 @@ lattices[-1].setLatticeCells(
      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
+lattices[-1].printString()
+
 
 # Top right, bottom left 17 x 17 assemblies 
 lattices.append(Lattice(id=21, width_x=1.26, width_y=1.26))
@@ -237,6 +239,8 @@ lattices[-1].setLatticeCells(
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7]])
+
+lattices[-1].printString()
 
 
 # Sliced up water cells - right side of geometry
@@ -281,6 +285,8 @@ lattices[-1].setLatticeCells(
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]])
+
+lattices[-1].printString()
 
 
 # Sliced up water cells for bottom of geometry
