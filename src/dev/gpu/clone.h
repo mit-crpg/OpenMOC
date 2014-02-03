002ee091efe71b767469620ef257d3b573de1c3b
Will Boyd <wbinventor@gmail.com>
2014-02-03 12:49:57 -0500

    Updated SWIG typemaps in interface files to allow for single or double precision multi-group cross-sections

--- src/dev/gpu/clone.h
+++
@@ -1,5 +0,0 @@
-#include "../DeviceMaterial.h"
-#include "../DeviceTrack.h"
-
-void cloneMaterialOnGPU(Material* material_h, dev_material* material_d);
-void cloneTrackOnGPU(Track* track_h, dev_track* track_d);
