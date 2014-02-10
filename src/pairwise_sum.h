cda8da7243722c38f2902c17987d78ed801c72a6
Will Boyd <wbinventor@gmail.com>
2014-02-10 14:13:17 -0500

    pairwise_sum.h now conforms to Style Guidelines

--- src/pairwise_sum.h
+++ src/pairwise_sum.h
@@ -18,24 +18,22 @@
 template <typename T>
 inline T pairwise_sum(T* vector, int length) {
 
-    T sum = 0;
+  T sum = 0;
 
-    /* Base case: if length is less than 16, perform summation */
-    if (length < 16) {
-        #pragma simd reduction(+:sum)
-	for (int i=0; i < length; i++)
-	    sum += vector[i];
-    }
+  /* Base case: if length is less than 16, perform summation */
+  if (length < 16) {
 
-    else {
-	int offset = length % 2;
-        length = floor(length / 2);
-	sum = pairwise_sum<T>(&vector[0], length) + 
-	      pairwise_sum<T>(&vector[length], length+offset);	
-    }
+    #pragma simd reduction(+:sum)
+    for (int i=0; i < length; i++)
+      sum += vector[i];
+  }
 
-    return sum;
+  else {
+    int offset = length % 2;
+    length = floor(length / 2);
+    sum = pairwise_sum<T>(&vector[0], length) +
+          pairwise_sum<T>(&vector[length], length+offset);
+  }
+
+  return sum;
 }
-#ifdef MIC
-#pragma offload_attribute(pop)
-#endif
