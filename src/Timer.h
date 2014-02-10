6434c507f9f6d3138f2e564893b5e7e140692858
Will Boyd <wbinventor@gmail.com>
2014-02-10 14:11:00 -0500

    Timer.cpp and Timer.h now conform to Style Guidelines

--- src/Timer.h
+++ src/Timer.h
@@ -24,69 +24,69 @@
 
 /**
  * @class Timer Timer.h "openmoc/src/host/Timer.cpp"
- * @brief The timer class is for timing and profiling regions of code.
+ * @brief The Timer class is for timing and profiling regions of code.
  */
 class Timer {
 
 private:
 
-    /** A vector of timespec struct pointers for the start time at
-     *  each recursive level at which we are timing */
-    static std::vector<double> _start_times;
+  /** A vector of floating point start times at each inclusive level
+   *  at which we are timing */
+  static std::vector<double> _start_times;
 
-    /** The time elapsed (seconds) for the current split */
-    float _elapsed_time;
+  /** The time elapsed (seconds) for the current split */
+  float _elapsed_time;
 
-    /** Whether or not the timer is running for the current split */
-    bool _running;
+  /** Whether or not the Timer is running for the current split */
+  bool _running;
 
-    /** A vector of the times and messages for each split */
-    static std::map<std::string, double> _timer_splits;
+  /** A vector of the times and messages for each split */
+  static std::map<std::string, double> _timer_splits;
 
-    /**
-     * @brief Assignment operator for static referencing of the Timer.
-     * @param & the Timer static class object
-     * @return a pointer to the Timer static class object
-     */
-    Timer &operator=(const Timer &) { return *this; }
+  /**
+   * @brief Assignment operator for static referencing of the Timer.
+   * @param & the Timer static class object
+   * @return a pointer to the Timer static class object
+   */
+  Timer &operator=(const Timer &) { return *this; }
 
-    /**
-     * @brief Timer constructor.
-     * @param & The Timer static reference pointer.
-     */
-    Timer(const Timer &) { }
+  /**
+   * @brief Timer constructor.
+   * @param & The Timer static reference pointer.
+   */
+  Timer(const Timer &) { }
 
 public:
-    /**
-     * @brief Constructor sets the current split elapsed time to zero.
-     */
-    Timer() {
-        _running = false;
-        _elapsed_time = 0;
-    }
+  /**
+   * @brief Constructor sets the current split elapsed time to zero.
+   */
+  Timer() {
+    _running = false;
+    _elapsed_time = 0;
+  }
 
-    /**
-     * @brief Destructor
-     */
-    virtual ~Timer() { }
+  /**
+   * @brief Destructor
+   */
+  virtual ~Timer() { }
 
-    /**
-     * @brief Returns a static instance of the Timer class.
-     * @return a pointer to the static Timer class
-     */
-    static Timer *Get() {
-        static Timer instance;
-	return &instance;
-    }
+  /**
+   * @brief Returns a static instance of the Timer class.
+   * @return a pointer to the static Timer class
+   */
+  static Timer *Get() {
+    static Timer instance;
+    return &instance;
+  }
 
-    void startTimer();
-    void stopTimer();
-    void recordSplit(const char* msg);
-    double getTime();
-    double getSplit(const char* msg);
-    void printSplits();
-    void clearSplit(const char* msg);
-    void clearSplits();
+  void startTimer();
+  void stopTimer();
+  void recordSplit(const char* msg);
+  double getTime();
+  double getSplit(const char* msg);
+  void printSplits();
+  void clearSplit(const char* msg);
+  void clearSplits();
 };
 
 #endif /* TIMER_H_ */
