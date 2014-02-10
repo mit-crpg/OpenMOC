cb024b87b46fb43ba87eb56344861ce19013abde
Will Boyd <wbinventor@gmail.com>
2014-02-10 14:04:08 -0500

    LocalCoords.h and LocalCoords.cpp now conform to Style Guidelines

--- src/LocalCoords.cpp
+++ src/LocalCoords.cpp
@@ -6,9 +6,9 @@
  * @param y the y-coordinate
  */
 LocalCoords::LocalCoords(double x, double y) {
-    _coords.setCoords(x, y);
-    _next = NULL;
-    _prev = NULL;
+  _coords.setCoords(x, y);
+  _next = NULL;
+  _prev = NULL;
 }
 
 
@@ -19,381 +19,381 @@ LocalCoords::~LocalCoords() { }
 
 
 /**
- * @brief Return the level (UNIV or LAT) of this localcoords.
- * @return the level (UNIV or LAT)
+ * @brief Return the level (UNIV or LAT) of this LocalCoords.
+ * @return the nested Universe level (UNIV or LAT)
  */
 coordType LocalCoords::getType() {
-    return _type;
+  return _type;
 }
 
 
 /**
- * @brief Return the id of the universe within which this localcoords resides.
- * @return the universe id
+ * @brief Return the ID of the Universe within which this LocalCoords resides.
+ * @return the Universe ID
  */
 int LocalCoords::getUniverse() const {
-    return _universe;
+  return _universe;
 }
 
 
 /**
- * @brief Return the id of the cell within which this localcoords resides.
- * @return the cell id
+ * @brief Return the ID of the Cell within which this LocalCoords resides.
+ * @return the Cell ID
  */
 int LocalCoords::getCell() const {
-    return _cell;
+  return _cell;
 }
 
 
 /**
- * @brief Return the id of the lattice within which this localcoords resides.
- * @return the lattice id
+ * @brief Return the ID of the Lattice within which this LocalCoords resides.
+ * @return the Lattice ID
  */
 int LocalCoords::getLattice() const {
-    return _lattice;
+  return _lattice;
 }
 
 /**
- * @brief Return the first index of the lattice cell within which this 
- *        localcoords resides.
- * @return the first lattice cell index
+ * @brief Return the first index of the Lattice cell within which this
+ *        LocalCoords resides.
+ * @return the first Lattice cell index
  */
 int LocalCoords::getLatticeX() const {
-    return _lattice_x;
+  return _lattice_x;
 }
 
 
 /**
- * @brief Return the second index of the lattice cell within which this
- *        localcoords resides.
- * @return the second lattice cell index
+ * @brief Return the second index of the Lattice cell within which this
+ *        LocalCoords resides.
+ * @return the second Lattice cell index
  */
 int LocalCoords::getLatticeY() const {
-    return _lattice_y;
+  return _lattice_y;
 }
 
 
 /**
- * @brief Returns the x-coordinate for this localcoords location.
- * @return the x-coordinate of this localcoords location
+ * @brief Returns the x-coordinate for this LocalCoords location.
+ * @return the x-coordinate of this LocalCoords location
  */
 double LocalCoords::getX() const {
-    return _coords.getX();
+  return _coords.getX();
 }
 
 
 /**
- * @brief Returns the y-coordinate for this localcoords location.
- * @return the y-coordinate of this localcoords location
+ * @brief Returns the y-coordinate for this LocalCoords location.
+ * @return the y-coordinate of this LocalCoords location
  */
 double LocalCoords::getY() const {
-    return _coords.getY();
+  return _coords.getY();
 }
 
 
 /**
- * @brief Returns a pointer to the point containing the coordinates for this
- *        localcoord.
- * @return pointer to the point containing the x and y coordinates
+ * @brief Returns a pointer to the Point containing the coordinates for this
+ *        LocalCoord.
+ * @return pointer to the Point containing the x and y coordinates
  */
 Point* LocalCoords::getPoint() {
-    return &_coords;
+  return &_coords;
 }
 
 
 /**
- * @brief Return a pointer to the localcoord at the next lower nested universe
+ * @brief Return a pointer to the LocalCoord at the next lower nested Universe
  *        level if one exists.
- * @return pointer to the next localcoord
+ * @return pointer to the next LocalCoord
  */
 LocalCoords* LocalCoords::getNext() const {
-    return _next;
+  return _next;
 }
 
 
 /**
- * @brief Return a pointer to the localcoord at the next higher nested universe
+ * @brief Return a pointer to the LocalCoord at the next higher nested Universe
  *        level if one exists.
- * @return pointer to the previous localcoord
+ * @return pointer to the previous LocalCoord
  */
 LocalCoords* LocalCoords::getPrev() const {
-    return _prev;
+  return _prev;
 }
 
 
 /**
- * @brief Set the type of localcoords (UNIV or LAT).
- * @param type the type for localcoords (UNIV or LAT)
+ * @brief Set the type of LocalCoords (UNIV or LAT).
+ * @param type the type for LocalCoords (UNIV or LAT)
  */
 void LocalCoords::setType(coordType type) {
-    _type = type;
+  _type = type;
 }
 
 
 /**
- * @brief Set the id of the universe within which this localcoords resides.
- * @param universe the universe id
+ * @brief Set the ID of the Universe within which this LocalCoords resides.
+ * @param universe the Universe ID
  */
 void LocalCoords::setUniverse(int universe) {
-    _universe = universe;
+  _universe = universe;
 }
 
 
 /**
- * @brief Set the id of the cell within which this localcoords resides.
- * @param cell the cell id
+ * @brief Set the ID of the Cell within which this LocalCoords resides.
+ * @param cell the Cell ID
  */
 void LocalCoords::setCell(int cell) {
-    _cell = cell;
+  _cell = cell;
 }
 
 
 /**
- * @brief Sets the id of the lattice within which this localcoords resides.
- * @param lattice the lattice id
+ * @brief Sets the ID of the Lattice within which this LocalCoords resides.
+ * @param lattice the Lattice ID
  */
 void LocalCoords::setLattice(int lattice) {
-    _lattice = lattice;
+  _lattice = lattice;
 }
 
 
 /**
- * @brief Sets the first index for the lattice cell within which this 
- *        localcoords resides.
- * @param lattice_x the first lattice cell index
+ * @brief Sets the row index for the Lattice cell within which this
+ *        LocalCoords resides.
+ * @param lattice_x the row Lattice cell index
  */
 void LocalCoords::setLatticeX(int lattice_x) {
-    _lattice_x = lattice_x;
+  _lattice_x = lattice_x;
 }
 
 
 /**
- * @brief Sets the second index for the lattice cell within which this 
- *        localcoords resides.
- * @param lattice_y the second lattice cell index
+ * @brief Sets the column index for the Lattice cell within which this
+ *        LocalCoords resides.
+ * @param lattice_y the column Lattice cell index
  */
 void LocalCoords::setLatticeY(int lattice_y) {
-    _lattice_y = lattice_y;
+  _lattice_y = lattice_y;
 }
 
 
 /**
- * @brief Set the x-coordinate for this localcoords.
+ * @brief Set the x-coordinate for this LocalCoords.
  * @param x the x-coordinate
  */
 void LocalCoords::setX(double x) {
-    _coords.setX(x);
+  _coords.setX(x);
 }
 
 
 /**
- * @brief Set the y-coordinate for this localcoords.
+ * @brief Set the y-coordinate for this Localcoords.
  * @param y the y-coordinate
  */
 void LocalCoords::setY(double y) {
-    _coords.setY(y);
+  _coords.setY(y);
 }
 
 
 /**
- * @brief Sets the pointer to the localcoords on the next lower nested universe
+ * @brief Sets the pointer to the LocalCoords on the next lower nested Universe
  *        level.
- * @param next pointer to the next localcoords
+ * @param next pointer to the next LocalCoords
  */
 void LocalCoords::setNext(LocalCoords* next) {
-    _next = next;
+  _next = next;
 }
 
 
 /**
- * @brief Sets the pointer to the localcoords on the next higher nested
- *        universe level.
- * @param prev pointer to the previous localcoords
+ * @brief Sets the pointer to the LocalCoords on the next higher nested
+ *        Universe level.
+ * @param prev pointer to the previous LocalCoords
  */
 void LocalCoords::setPrev(LocalCoords* prev) {
-    _prev = prev;
+  _prev = prev;
 }
 
 
 /**
- * @brief Find and return the last localcoords in the linked list wich 
+ * @brief Find and return the last LocalCoords in the linked list wich
  *        represents the local coordinates on the lowest level of a geometry
  *        of nested universes.
- * @details Traverses a linked list of localcoords to find the one at the
- *          lowest universe level.
- * @return a pointer to the last localcoords object in the list
+ * @details Traverses a linked list of LocalCoords to find the one at the
+ *          lowest nested Universe level.
+ * @return a pointer to the last LocalCoords object in the list
  */
 LocalCoords* LocalCoords::getLowestLevel() {
-    LocalCoords* curr = this;
+  LocalCoords* curr = this;
 
-    if (curr)
+  if (curr)
 
     /* Traverse linked list */
     while (curr->getNext() != NULL)
-        curr = curr->getNext();
+      curr = curr->getNext();
 
-    return curr;
+  return curr;
 }
 
 
 /**
- * @brief Translate all of the x,y coordinates for each localcoords object in 
- *        the linked list. 
- * @details This method will traverse the entire linked list and apply the 
+ * @brief Translate all of the x,y coordinates for each LocalCoords object in
+ *        the linked list.
+ * @details This method will traverse the entire linked list and apply the
  *          translation to each element.
  * @param delta_x amount we wish to move x by
  * @param delta_y amount we wish to move y by
  */
 void LocalCoords::adjustCoords(double delta_x, double delta_y) {
 
-    /* Forward direction along linked list */
-    LocalCoords* curr = this;
-    while (curr != NULL) {
-        curr->setX(curr->getX() + delta_x);
-	curr->setY(curr->getY() + delta_y);
-	curr = curr->getNext();
-    }
+  /* Forward direction along linked list */
+  LocalCoords* curr = this;
+  while (curr != NULL) {
+    curr->setX(curr->getX() + delta_x);
+    curr->setY(curr->getY() + delta_y);
+    curr = curr->getNext();
+  }
 
-    /* Reverse direction along linked list */
-    curr = _prev;
-    while (curr != NULL) {
-        curr->setX(curr->getX() + delta_x);
-	curr->setY(curr->getY() + delta_y);
-	curr = curr->getPrev();
-    }
-    return;
+  /* Reverse direction along linked list */
+  curr = _prev;
+  while (curr != NULL) {
+    curr->setX(curr->getX() + delta_x);
+    curr->setY(curr->getY() + delta_y);
+    curr = curr->getPrev();
+  }
+  return;
 }
 
 
 /**
- * @brief Update the last element in the linked list (the one at the lowest 
- *        level of nested universes) to have the same coordinates as a 
+ * @brief Update the last element in the linked list (the one at the lowest
+ *        level of nested Universes) to have the same coordinates as a
  *        given point.
  * @param point a pointer to a point of interest
  */
 void LocalCoords::updateMostLocal(Point* point) {
 
-    /* Get the lowest level coordinate */
-    LocalCoords* curr = getLowestLevel();
+  /* Get the lowest level coordinate */
+  LocalCoords* curr = getLowestLevel();
 
-    /* Translate coordinates by appropriate amount */
-    double delta_x = point->getX() - curr->getX();
-    double delta_y = point->getY() - curr->getY();
-    adjustCoords(delta_x, delta_y);
+  /* Translate coordinates by appropriate amount */
+  double delta_x = point->getX() - curr->getX();
+  double delta_y = point->getY() - curr->getY();
+  adjustCoords(delta_x, delta_y);
 
-    return;
+  return;
 }
 
 
 /**
- * @brief Removes and frees memory for all localcoords beyond this one
+ * @brief Removes and frees memory for all LocalCoords beyond this one
  *        in the linked list
  */
 void LocalCoords::prune() {
 
-    LocalCoords* curr = getLowestLevel();
-    LocalCoords* next = curr->getPrev();
+  LocalCoords* curr = getLowestLevel();
+  LocalCoords* next = curr->getPrev();
 
-    /* Iterate over localcoords beneath this one in the linked list */
-    while (curr != this) {
-        next = curr->getPrev();
-	delete curr;
-	curr = next;
-    }
+  /* Iterate over LocalCoords beneath this one in the linked list */
+  while (curr != this) {
+    next = curr->getPrev();
+    delete curr;
+    curr = next;
+  }
 
-    /* Set the next localcoord in the linked list to null */
-    setNext(NULL);
+  /* Set the next LocalCoord in the linked list to null */
+  setNext(NULL);
 }
 
 
 /**
- * @brief Copies a localcoords' values to this one.
- * details Given a pointer to a localcoords, it first prunes it and then creates
- *         a copy of the linked list of localcoords in the linked list below 
- *         this one to give to the input localcoords.
- * @param coords a pointer to the localcoords to give the linked list copy to
+ * @brief Copies a LocalCoords' values to this one.
+ * details Given a pointer to a LocalCoords, it first prunes it and then creates
+ *         a copy of the linked list of LocalCoords in the linked list below
+ *         this one to give to the input LocalCoords.
+ * @param coords a pointer to the LocalCoords to give the linked list copy to
  */
 void LocalCoords::copyCoords(LocalCoords* coords) {
 
-    LocalCoords* curr1 = this;
-    LocalCoords* curr2 = coords;
+  LocalCoords* curr1 = this;
+  LocalCoords* curr2 = coords;
 
-    /* Prune the localcoords linked list */
-    curr2->prune();
+  /* Prune the LocalCoords linked list */
+  curr2->prune();
 
-    /* Iterate over this localcoords linked list and create a
-     * copy of it for the input localcoords */
-    while (curr1 != NULL) {
-        curr2->setX(curr1->getX());
-	curr2->setY(curr1->getY());
-	curr2->setUniverse(curr1->getUniverse());
+  /* Iterate over this LocalCoords linked list and create a
+   * copy of it for the input LocalCoords */
+  while (curr1 != NULL) {
+    curr2->setX(curr1->getX());
+    curr2->setY(curr1->getY());
+    curr2->setUniverse(curr1->getUniverse());
 
-	if (curr1->getType() == UNIV) {
-	    curr2->setType(UNIV);
-	    curr2->setCell(curr1->getCell());
-	}
-	else {
-	    curr2->setLattice(curr1->getLattice());
-	    curr2->setLatticeX(curr1->getLatticeX());
-	    curr2->setLatticeY(curr1->getLatticeY());
-	    curr2->setType(LAT);
-	}
-
-	curr1 = curr1->getNext();
-	
-	if (curr1 != NULL && curr2->getNext() == NULL) {
-	    LocalCoords* new_coords = new LocalCoords(0.0, 0.0);
-	    curr2->setNext(new_coords);
-	    new_coords->setPrev(curr2);
-	    curr2 = new_coords;
-	}
-	else if (curr1 != NULL)
-	    curr2 = curr2->getNext();
+    if (curr1->getType() == UNIV) {
+      curr2->setType(UNIV);
+      curr2->setCell(curr1->getCell());
+    }
+    else {
+      curr2->setLattice(curr1->getLattice());
+      curr2->setLatticeX(curr1->getLatticeX());
+      curr2->setLatticeY(curr1->getLatticeY());
+      curr2->setType(LAT);
     }
 
-    /* Prune any remainder from the old coords linked list */
-    if (curr2 != NULL)
-        curr2->prune();
+    curr1 = curr1->getNext();
+
+    if (curr1 != NULL && curr2->getNext() == NULL) {
+      LocalCoords* new_coords = new LocalCoords(0.0, 0.0);
+      curr2->setNext(new_coords);
+      new_coords->setPrev(curr2);
+      curr2 = new_coords;
+    }
+    else if (curr1 != NULL)
+      curr2 = curr2->getNext();
+  }
+
+  /* Prune any remainder from the old coords linked list */
+  if (curr2 != NULL)
+    curr2->prune();
 }
 
 
 /**
- * @brief Converts this localcoords's attributes to a character array 
+ * @brief Converts this LocalCoords's attributes to a character array
  *        representation.
- * @return a character array of its member's attributes
+ * @return a character array of the LocalCoord's attributes
  */
 std::string LocalCoords::toString() {
 
-    std::stringstream string;
-    LocalCoords* curr = this;
+  std::stringstream string;
+  LocalCoords* curr = this;
 
-    /* Loops over all localcoords lower than this one in the list */
-    while (curr != NULL) {
-        string << "LocalCoords: level = ";
+  /* Loops over all localcoords lower than this one in the list */
+  while (curr != NULL) {
+    string << "LocalCoords: level = ";
 
-	if (curr->getType() == UNIV) {
-	    string << " UNIVERSE, x = " << curr->getX() << ", y = " 
-		   << curr->getY() << ", universe = " << curr->getUniverse() 
-		   << ", cell = " << curr->getCell();
-	}
-	else if (curr->getType() == LAT){
-	    string << " LATTICE, x = " << curr->getX() << ", y = " 
-		   << curr->getY() << ", universe = " << curr->getUniverse() 
-		   << ", lattice = " << curr->getLattice() << ", lattice_x = " 
-		   << curr->getLatticeX() << ", lattice_y = " 
-		   << curr->getLatticeY();
-	}
-	else {
-	    string << " NONE, x = " << curr->getX() << ", y = " << curr->getY()
-		   << ", universe = " << curr->getUniverse() << ", lattice = " 
-		   << curr->getLattice() << ", lattice_x = " 
-		   << curr->getLatticeX() << ", lattice_y = " 
-		   << curr->getLatticeY() << ", cell = " << curr->getCell();
-	}
-
-	string << ", next:\n";
-        curr = curr->getNext();
+    if (curr->getType() == UNIV) {
+      string << " UNIVERSE, x = " << curr->getX() << ", y = "
+             << curr->getY() << ", universe = " << curr->getUniverse()
+             << ", cell = " << curr->getCell();
+    }
+    else if (curr->getType() == LAT){
+      string << " LATTICE, x = " << curr->getX() << ", y = "
+             << curr->getY() << ", universe = " << curr->getUniverse()
+             << ", lattice = " << curr->getLattice() << ", lattice_x = "
+             << curr->getLatticeX() << ", lattice_y = "
+             << curr->getLatticeY();
+    }
+    else {
+      string << " NONE, x = " << curr->getX() << ", y = " << curr->getY()
+             << ", universe = " << curr->getUniverse() << ", lattice = "
+             << curr->getLattice() << ", lattice_x = "
+             << curr->getLatticeX() << ", lattice_y = "
+             << curr->getLatticeY() << ", cell = " << curr->getCell();
     }
 
-    return string.str();
+    string << ", next:\n";
+    curr = curr->getNext();
+  }
+
+  return string.str();
 }
--- src/LocalCoords.h
+++ src/LocalCoords.h
@@ -15,77 +15,87 @@
 
 /**
  * @enum coordType
- * @brief The type of universe level on which the localcoords reside
+ * @brief The type of Universe level on which the LocalCoords reside
  */
 enum coordType {
-    /** A universe level coordinate type */
-    UNIV,
-    /** A lattice level coordinate type */
-    LAT
+  /** A Universe level coordinate type */
+  UNIV,
+
+  /** A Lattice level coordinate type */
+  LAT
 };
 
 
 /**
  * @class LocalCoords LocalCoords.h "openmoc/src/host/LocalCoords.h"
- * @brief The localcoords represents a set of local coordinates on some
- *        level of nested universes making up the geometry.
+ * @brief The LocalCoords represents a set of local coordinates on some
+ *        level of nested Universes making up the geometry.
  */
 class LocalCoords {
 
 private:
-    /** The local coordinate type (UNIV or LAT) */
-    coordType _type;
-    /** The id of the universe within which this localcoords resides */
-    int _universe;
-    /** The id of the cell within which this localcoords resides */
-    int _cell;
-    /** The id of the lattice within which this localcoords resides */
-    int _lattice;
-    /** The first index of the lattice cell within which this localcoords 
-     *  resides */
-    int _lattice_x;
-    /** The second index of the lattice cell within which this localcoords 
-     *  resides */
-    int _lattice_y;
-    /** A point representing the 2D coordinates of this localcoords */
-    Point _coords;
-    /** A pointer to the localcoords at the next lower nested universe level */
-    LocalCoords* _next;
-    /** A pointer to the localcoords at the next higher nested universe level */
-    LocalCoords* _prev;
+  /** The local coordinate type (UNIV or LAT) */
+  coordType _type;
+
+  /** The ID of the Universe within which this LocalCoords resides */
+  int _universe;
+
+  /** The ID of the Cell within which this LocalCoords resides */
+  int _cell;
+
+  /** The ID of the Lattice within which this LocalCoords resides */
+  int _lattice;
+
+  /** The first index of the Lattice cell within which this LocalCoords
+   *  resides */
+  int _lattice_x;
+
+  /** The second index of the Lattice cell within which this LocalCoords
+   *  resides */
+  int _lattice_y;
+
+  /** A Point representing the 2D coordinates of this LocalCoords */
+  Point _coords;
+
+  /** A pointer to the LocalCoords at the next lower nested Universe level */
+  LocalCoords* _next;
+
+  /** A pointer to the LocalCoords at the next higher nested Universe level */
+  LocalCoords* _prev;
 
 public:
-    LocalCoords(double x, double y);
-    virtual ~LocalCoords();
-    coordType getType();
-    int getUniverse() const;
-    int getCell() const;
-    int getLattice() const;
-    int getLatticeX() const;
-    int getLatticeY() const;
-    double getX() const;
-    double getY() const;
-    Point* getPoint();
-    LocalCoords* getNext() const;
-    LocalCoords* getPrev() const;
+  LocalCoords(double x, double y);
+  virtual ~LocalCoords();
+  coordType getType();
+  int getUniverse() const;
+  int getCell() const;
+  int getLattice() const;
+  int getLatticeX() const;
+  int getLatticeY() const;
+  double getX() const;
+  double getY() const;
+  Point* getPoint();
+  LocalCoords* getNext() const;
+  LocalCoords* getPrev() const;
 
-    void setType(coordType type);
-    void setUniverse(int universe);
-    void setCell(int cell);
-    void setLattice(int lattice);
-    void setLatticeX(int lattice_x);
-    void setLatticeY(int lattice_y);
-    void setX(double x);
-    void setY(double y);
-    void setNext(LocalCoords *next);
-    void setPrev(LocalCoords* coords);
+  void setType(coordType type);
+  void setUniverse(int universe);
+  void setCell(int cell);
+  void setLattice(int lattice);
+  void setLatticeX(int lattice_x);
+  void setLatticeY(int lattice_y);
+  void setX(double x);
+  void setY(double y);
+  void setNext(LocalCoords *next);
+  void setPrev(LocalCoords* coords);
 
-    LocalCoords* getLowestLevel();
-    void adjustCoords(double delta_x, double delta_y);
-    void updateMostLocal(Point* point);
-    void prune();
-    void copyCoords(LocalCoords* coords);
-    std::string toString();
+  LocalCoords* getLowestLevel();
+  void adjustCoords(double delta_x, double delta_y);
+  void updateMostLocal(Point* point);
+  void prune();
+  void copyCoords(LocalCoords* coords);
+  std::string toString();
 };
+
 
 #endif /* LOCALCOORDS_H_ */
