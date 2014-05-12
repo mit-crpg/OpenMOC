8a9026c7786b3420bc67927c080f171793cd7a1f
Stephanie A Pavlick <spavlick@mit.edu>
2014-05-12 12:48:01 -0400

    Fixed bugs in average calculator

--- openmoc/compatible/casmo.py
+++ openmoc/compatible/casmo.py
@@ -713,38 +713,38 @@ class Casmo(object):
     val_dict = {}
     for xs_type in xs_types:
       for region in regions:
-        val_dict[xs_type][region] = []
+        val_dict[region][xs_type] = []
     
     for i in range(len(self._cell_type_array)):
       for j in range(len(self._cell_type_array[i])):
         if string_pin_cell_array[i][j]=='gt':
           for xs_type in xs_types:
-            val_dict[xs_type]['water'].append(variable_dict[xs_type][self._min_microregions[i][j])
-            val_dict[xs_type]['cladding'].append(variable_dict[xs_type][self._min_microregions[i][j]+1)
+            val_dict['water'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]])
+            val_dict['cladding'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+1])
             for k in range(self._min_microregions[i][j]+2,self.max_microregions[i][j]+1):
-              val_dict[xs_type]['water'].append(variable_dict[xs_type][k])
+              val_dict['water'][xs_type].append(variable_dict[xs_type][k])
         elif string_pin_cell_array[i][j]=='fuel':
           for xs_type in xs_types:
-            val_dict[xs_type]['fuel'].append(variable_dict[xs_type][self._min_microregions[i][j])
-            val_dict[xs_type]['helium'].append(variable_dict[xs_type][self._min_microregions[i][j]+1)
-            val_dict[xs_type]['cladding'].append(variable_dict[xs_type][self._min_microregions[i][j]+2)
+            val_dict['fuel'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]])
+            val_dict['helium'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+1])
+            val_dict['cladding'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+2])
             for k in range(self._min_microregions[i][j]+3,self.max_microregions[i][j]+1):
               val_dict[xs_type]['water'].append(variable_dict[xs_type][k])
         elif string_pin_cell_array[i][j]=='bp':
           for xs_type in xs_types:
-            val_dict[xs_type]['helium'].append(variable_dict[xs_type][self._min_microregions[i][j])
-            val_dict[xs_type]['ss304'].append(variable_dict[xs_type][self._min_microregions[i][j]+1)
-            val_dict[xs_type]['helium'].append(variable_dict[xs_type][self._min_microregions[i][j]+2)
-            val_dict[xs_type]['bp'].append(variable_dict[xs_type][self._min_microregions[i][j]+3)
-            val_dict[xs_type]['helium'].append(variable_dict[xs_type][self._min_microregions[i][j]+4)
-            val_dict[xs_type]['ss304'].append(variable_dict[xs_type][self._min_microregions[i][j]+5)
-            val_dict[xs_type]['water'].append(variable_dict[xs_type][self._min_microregions[i][j]+6)
-            val_dict[xs_type]['zircaloy'].append(variable_dict[xs_type][self._min_microregions[i][j]+7)
+            val_dict['helium'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]])
+            val_dict['ss304'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+1])
+            val_dict['helium'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+2])
+            val_dict['bp'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+3])
+            val_dict['helium'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+4])
+            val_dict['ss304'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+5])
+            val_dict['water'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+6])
+            val_dict['zircaloy'][xs_type].append(variable_dict[xs_type][self._min_microregions[i][j]+7])
             for k in range(self._min_microregions[i][j]+8,self.max_microregions[i][j]+1):
-              val_dict[xs_type]['water'].append(variable_dict[xs_type][k])
+              val_dict['water'][xs_type].append(variable_dict[xs_type][k])
         
     avg_dict = {}            
     for xs_type in xs_types:
       for region in regions:
-        avg_dict[xs_type][region] = sum(val_dict[xs_type][region])/len(val_dict[xs_type][region])
+        avg_dict[region][xs_type] = sum(val_dict[region][xs_type])/len(val_dict[region][xs_type])
     self._average_cross_sections = avg_dict
