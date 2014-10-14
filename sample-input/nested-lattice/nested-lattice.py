090f4cac3bfc23c77e4b8fc3f0df01b9ad366291
Will Boyd <wbinventor@gmail.com>
2014-10-14 14:27:30 -0400

    Implemented nested-lattice.py using refactored Geometry

--- sample-input/nested-lattice/nested-lattice.py
+++ sample-input/nested-lattice/nested-lattice.py
@@ -28,9 +28,6 @@ log.py_printf('NORMAL', 'Importing materials data from HDF5...')
 
 materials = materialize.materialize('../c5g7-materials.h5')
 
-uo2_id = materials['UO2'].getId()
-water_id = materials['Water'].getId()
-
 
 ###############################################################################
 ###########################   Creating Surfaces   #############################
@@ -38,15 +35,15 @@ water_id = materials['Water'].getId()
 
 log.py_printf('NORMAL', 'Creating surfaces...')
 
-circles = []
-planes = []
-planes.append(XPlane(x=-2.0))
-planes.append(XPlane(x=2.0))
-planes.append(YPlane(y=-2.0))
-planes.append(YPlane(y=2.0))
-circles.append(Circle(x=0.0, y=0.0, radius=0.4))
-circles.append(Circle(x=0.0, y=0.0, radius=0.3))
-circles.append(Circle(x=0.0, y=0.0, radius=0.2))
+circles = list()
+planes = list()
+planes.append(XPlane(x=-2.0, name='left'))
+planes.append(XPlane(x=2.0, name='right'))
+planes.append(YPlane(y=-2.0, name='top'))
+planes.append(YPlane(y=2.0, name='bottom'))
+circles.append(Circle(x=0.0, y=0.0, radius=0.4, name='large pin'))
+circles.append(Circle(x=0.0, y=0.0, radius=0.3, name='medium pin'))
+circles.append(Circle(x=0.0, y=0.0, radius=0.2, name='small pin'))
 for plane in planes: plane.setBoundaryType(REFLECTIVE)
 
 
@@ -56,15 +53,23 @@ for plane in planes: plane.setBoundaryType(REFLECTIVE)
 
 log.py_printf('NORMAL', 'Creating cells...')
 
-cells = []
-cells.append(CellBasic(universe=1, material=uo2_id))
-cells.append(CellBasic(universe=1, material=water_id))
-cells.append(CellBasic(universe=2, material=uo2_id))
-cells.append(CellBasic(universe=2, material=water_id))
-cells.append(CellBasic(universe=3, material=uo2_id))
-cells.append(CellBasic(universe=3, material=water_id))
-cells.append(CellFill(universe=5, universe_fill=4))
-cells.append(CellFill(universe=0, universe_fill=6))
+cells = list()
+cells = list()
+cells.append(CellBasic(name='large pin fuel', rings=3, sectors=8))
+cells.append(CellBasic(name='large pin moderator', sectors=8))
+cells.append(CellBasic(name='medium pin fuel', rings=3, sectors=8))
+cells.append(CellBasic(name='medium pin moderator', sectors=8))
+cells.append(CellBasic(name='small pin fuel', rings=3, sectors=8))
+cells.append(CellBasic(name='small pin moderator', sectors=8))
+cells.append(CellFill(name='lattice cell'))
+cells.append(CellFill(name='root cell'))
+
+cells[0].setMaterial(materials['UO2'])
+cells[1].setMaterial(materials['Water'])
+cells[2].setMaterial(materials['UO2'])
+cells[3].setMaterial(materials['Water'])
+cells[4].setMaterial(materials['UO2'])
+cells[5].setMaterial(materials['Water'])
 
 cells[0].addSurface(halfspace=-1, surface=circles[0])
 cells[1].addSurface(halfspace=+1, surface=circles[0])
@@ -80,18 +85,44 @@ cells[7].addSurface(halfspace=-1, surface=planes[3])
 
 
 ###############################################################################
+#                            Creating Universes
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating universes...')
+
+pin1 = Universe(name='large pin cell')
+pin2 = Universe(name='medium pin cell')
+pin3 = Universe(name='small pin cell')
+assembly = Universe(name='2x2 lattice')
+root = Universe(name='root universe')
+
+pin1.addCell(cells[0])
+pin1.addCell(cells[1])
+pin2.addCell(cells[2])
+pin2.addCell(cells[3])
+pin3.addCell(cells[4])
+pin3.addCell(cells[5])
+assembly.addCell(cells[6])
+root.addCell(cells[7])
+
+
+###############################################################################
 ###########################   Creating Lattices   #############################
 ###############################################################################
 
 log.py_printf('NORMAL', 'Creating nested 2 x 2 lattices...')
 
 # 2x2 assembly
-assembly = Lattice(id=4, width_x=1.0, width_y=1.0)
-assembly.setLatticeCells([[1, 2], [1, 3]])
+lattice = Lattice(name='2x2 lattice')
+lattice.setWidth(width_x=1.0, width_y=1.0)
+lattice.setUniverses([[pin1, pin2], [pin1, pin3]])
+cells[6].setFill(lattice)
 
 # 2x2 core
-core = Lattice(id=6, width_x=2.0, width_y=2.0)
-core.setLatticeCells([[5, 5], [5, 5]])
+core = Lattice(name='2x2 core')
+core.setWidth(width_x=2.0, width_y=2.0)
+core.setUniverses([[assembly, assembly], [assembly, assembly]])
+cells[7].setFill(core)
 
 
 ###############################################################################
@@ -101,11 +132,7 @@ core.setLatticeCells([[5, 5], [5, 5]])
 log.py_printf('NORMAL', 'Creating geometry...')
 
 geometry = Geometry()
-for material in materials.values(): geometry.addMaterial(material)
-for cell in cells: geometry.addCell(cell)
-geometry.addLattice(assembly)
-geometry.addLattice(core)
-
+geometry.setRootUniverse(root)
 geometry.initializeFlatSourceRegions()
 
 
@@ -138,7 +165,7 @@ log.py_printf('NORMAL', 'Plotting data...')
 
 #plotter.plot_tracks(track_generator)
 #plotter.plot_segments(track_generator)
-#plotter.plot_mterials(geometry, gridsize=500)
+#plotter.plot_materials(geometry, gridsize=500)
 #plotter.plot_cells(geometry, gridsize=500)
 #plotter.plot_flat_source_regions(geometry, gridsize=500)
 #plotter.plot_fluxes(geometry, solver, energy_groups=[1,2,3,4,5,6,7])
