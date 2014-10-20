added chi normalization and fixed process pin powers

openmoc/plotter.py
@@ -482,8 +482,8 @@ def plot_flat_source_regions(geometry, gridsize=250):
 # @param geometry a geometry object which has been initialized with Materials,
 #        Cells, Universes and Lattices. Segments must have been created or 
 #        extracted from a file.
-# @param cmfd a cmfd object which has been used with the geometry in 
-#        generating segments. The cmfd object must have the _overlay_mesh
+# @param cmfd a Cmfd object which has been used with the geometry in 
+#        generating segments. The Cmfd object must have the _overlay_mesh
 #        flag set to true; otherwise, the map linking FSR IDs to CMFD cells
 #        would not have been created.
 # @param gridsize an optional number of grid cells for the plot
@@ -499,24 +499,24 @@ def plot_cmfd_cells(geometry, cmfd, gridsize=250):
 
   # Error checking
   if not 'Geometry' in str(type(geometry)):
-    py_printf('ERROR', 'Unable to plot the cmfd cells since ' + \
+    py_printf('ERROR', 'Unable to plot the CMFD cells since ' + \
               'input was not a geometry class object')
 
   if not 'Cmfd' in str(type(cmfd)):
-    py_printf('ERROR', 'Unable to plot the cmfd cells since ' + \
-              'input was not a cmfd class object')
+    py_printf('ERROR', 'Unable to plot the CMFD cells since ' + \
+              'input was not a CMFD class object')
   
   if not isinstance(gridsize, int):
-    py_printf('ERROR', 'Unable to plot the cmfd cells since ' + \
+    py_printf('ERROR', 'Unable to plot the CMFD cells since ' + \
               'since the gridsize %s is not an integer', str(gridsize))
 
   if gridsize <= 0:
-    py_printf('Error', 'Unable to plot the cmfd cells ' + \
+    py_printf('Error', 'Unable to plot the CMFD cells ' + \
               'with a negative gridsize (%d)', gridsize)
 
-  py_printf('NORMAL', 'Plotting the cmfd cells...')
+  py_printf('NORMAL', 'Plotting the CMFD cells...')
 
-  # Get the number of flat source regions
+  # Get the number of CMFD cells
   num_cells = cmfd.getNumCells()
 
   # Create array of equally spaced randomized floats as a color map for plots
@@ -538,7 +538,7 @@ def plot_cmfd_cells(geometry, cmfd, gridsize=250):
   xcoords = np.linspace(xmin, xmax, gridsize)
   ycoords = np.linspace(ymin, ymax, gridsize)
 
-  # Find the cmfd cell ID for each grid point
+  # Find the CMFD cell ID for each grid point
   for i in range(gridsize):
     for j in range(gridsize):
 
@@ -556,7 +556,7 @@ def plot_cmfd_cells(geometry, cmfd, gridsize=250):
   # orientation expected by the user
   surface = np.flipud(surface)
 
-  # Plot a 2D color map of the cmfd cells
+  # Plot a 2D color map of the CMFD cells
   fig = plt.figure()
   plt.imshow(surface, extent=[xmin, xmax, ymin, ymax])
   plt.title('CMFD cells')