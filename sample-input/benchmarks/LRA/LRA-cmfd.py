2efd334614abb257497f08100758e89961688ad2
Will Boyd <wbinventor@gmail.com>
2015-01-12 21:14:13 -0500

    Refactored LRA input files

--- sample-input/benchmarks/LRA/LRA-cmfd.py
+++ sample-input/benchmarks/LRA/LRA-cmfd.py
@@ -37,87 +37,89 @@ materials = materialize.materialize('LRA-materials.py')
 
 log.py_printf('NORMAL', 'Creating surfaces...')
 
-planes = []
-planes.append(XPlane(x=-82.5))
-planes.append(XPlane(x=82.5))
-planes.append(YPlane(y=-82.5))
-planes.append(YPlane(y=82.5))
-planes[0].setBoundaryType(REFLECTIVE)
-planes[1].setBoundaryType(VACUUM)
-planes[2].setBoundaryType(REFLECTIVE)
-planes[3].setBoundaryType(VACUUM)
+left = XPlane(x=-82.5)
+right = XPlane(x=82.5)
+bottom = YPlane(y=-82.5)
+top = YPlane(y=82.5)
+left.setBoundaryType(REFLECTIVE)
+right.setBoundaryType(VACUUM)
+bottom.setBoundaryType(REFLECTIVE)
+top.setBoundaryType(VACUUM)
 
 
 ###############################################################################
-#############################   Creating Cells   ##############################
+######################   Creating Cells and Universes   #######################
 ###############################################################################
 
 log.py_printf('NORMAL', 'Creating cells...')
 
-cells = []
-cells.append(CellBasic())
-cells.append(CellBasic())
-cells.append(CellBasic())
-cells.append(CellBasic())
-cells.append(CellBasic())
-cells.append(CellBasic())
-cells.append(CellFill())
-cells.append(CellFill())
-cells.append(CellFill())
-cells.append(CellFill())
-cells.append(CellFill())
-cells.append(CellFill())
-cells.append(CellFill())
+# Region 1
+region1_cell = CellBasic(name='region 1')
+region1_cell.setMaterial(materials['region_1'])
+region1 = Universe(name='region 1')
+region1.addCell(region1_cell)
 
-cells[0].setMaterial(materials['region_1'])
-cells[1].setMaterial(materials['region_2'])
-cells[2].setMaterial(materials['region_3'])
-cells[3].setMaterial(materials['region_4'])
-cells[4].setMaterial(materials['region_5'])
-cells[5].setMaterial(materials['region_6'])
+# Region 2
+region2_cell = CellBasic(name='region 2')
+region2_cell.setMaterial(materials['region_2'])
+region2 = Universe(name='region 2')
+region2.addCell(region2_cell)
 
-cells[12].addSurface(halfspace=+1, surface=planes[0])
-cells[12].addSurface(halfspace=-1, surface=planes[1])
-cells[12].addSurface(halfspace=+1, surface=planes[2])
-cells[12].addSurface(halfspace=-1, surface=planes[3])
+# Region 3
+region3_cell = CellBasic(name='region 3')
+region3_cell.setMaterial(materials['region_3'])
+region3 = Universe(name='region 3')
+region3.addCell(region3_cell)
 
+# Region 4
+region4_cell = CellBasic(name='region 4')
+region4_cell.setMaterial(materials['region_4'])
+region4 = Universe(name='region 4')
+region4.addCell(region4_cell)
 
-###############################################################################
-###########################   Creating Universes   ############################
-###############################################################################
+# Region 5
+region5_cell = CellBasic(name='region 5')
+region5_cell.setMaterial(materials['region_5'])
+region5 = Universe(name='region 5')
+region5.addCell(region5_cell)
 
-log.py_printf('NORMAL', 'Creating universes...')
+# Region 5
+region6_cell = CellBasic(name='region 6')
+region6_cell.setMaterial(materials['region_6'])
+region6 = Universe(name='region 6')
+region6.addCell(region6_cell)
 
-universes = list()
-universes.append(Universe(name='region 1'))
-universes.append(Universe(name='region 2'))
-universes.append(Universe(name='region 3'))
-universes.append(Universe(name='region 4'))
-universes.append(Universe(name='region 5'))
-universes.append(Universe(name='region 6'))
+# CellFills
+assembly1_cell = CellFill(name='assembly 1')
+assembly2_cell = CellFill(name='assembly 2')
+assembly3_cell = CellFill(name='assembly 3')
+assembly4_cell = CellFill(name='assembly 4')
+assembly5_cell = CellFill(name='assembly 5')
+assembly6_cell = CellFill(name='assembly 6')
 
-universes.append(Universe(name='assembly 1'))
-universes.append(Universe(name='assembly 2'))
-universes.append(Universe(name='assembly 3'))
-universes.append(Universe(name='assembly 4'))
-universes.append(Universe(name='assembly 5'))
-universes.append(Universe(name='assembly 6'))
-universes.append(Universe(name='core'))
-root = Universe(name='root universe')
+assembly1 = Universe(name='assembly 1')
+assembly2 = Universe(name='assembly 2')
+assembly3 = Universe(name='assembly 3')
+assembly4 = Universe(name='assembly 4')
+assembly5 = Universe(name='assembly 5')
+assembly6 = Universe(name='assembly 6')
 
-universes[0].addCell(cells[0])
-universes[1].addCell(cells[1])
-universes[2].addCell(cells[2])
-universes[3].addCell(cells[3])
-universes[4].addCell(cells[4])
-universes[5].addCell(cells[5])
-universes[6].addCell(cells[6])
-universes[7].addCell(cells[7])
-universes[8].addCell(cells[8])
-universes[9].addCell(cells[9])
-universes[10].addCell(cells[10])
-universes[11].addCell(cells[11])
-root.addCell(cells[12])
+assembly1.addCell(assembly1_cell)
+assembly2.addCell(assembly2_cell)
+assembly3.addCell(assembly3_cell)
+assembly4.addCell(assembly4_cell)
+assembly5.addCell(assembly5_cell)
+assembly6.addCell(assembly6_cell)
+
+# Root cell/universe
+root_cell = CellFill(name='root cell')
+root_cell.addSurface(halfspace=+1, surface=left)
+root_cell.addSurface(halfspace=-1, surface=right)
+root_cell.addSurface(halfspace=+1, surface=bottom)
+root_cell.addSurface(halfspace=-1, surface=top)
+
+root_universe = Universe(name='root universe')
+root_universe.addCell(root_cell)
 
 
 ###############################################################################
@@ -126,121 +128,54 @@ root.addCell(cells[12])
 
 log.py_printf('NORMAL', 'Creating LRA lattices...')
 
-assembly1 = Lattice(name='assembly 1')
-assembly1.setWidth(width_x=1.5, width_y=1.5)
-template = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+# Assembly 1
+assembly1_lattice = Lattice(name='assembly 1')
+assembly1_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region1] * 10] * 10
+assembly1_lattice.setUniverses(template)
+assembly1_cell.setFill(assembly1_lattice)
 
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly1.setUniverses(template)
+# Assembly 2
+assembly2_lattice = Lattice(name='assembly 2')
+assembly2_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region2] * 10] * 10
+assembly2_lattice.setUniverses(template)
+assembly2_cell.setFill(assembly2_lattice)
 
+# Assembly 3
+assembly3_lattice = Lattice(name='assembly 3')
+assembly3_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region3] * 10] * 10
+assembly3_lattice.setUniverses(template)
+assembly3_cell.setFill(assembly3_lattice)
 
-assembly2 = Lattice(name='assembly 2')
-assembly2.setWidth(width_x=1.5, width_y=1.5)
-template = [[2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+# Assembly 4
+assembly4_lattice = Lattice(name='assembly 4')
+assembly4_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region4] * 10] * 10
+assembly4_lattice.setUniverses(template)
+assembly4_cell.setFill(assembly4_lattice)
 
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly2.setUniverses(template)
+# Assembly 5
+assembly5_lattice = Lattice(name='assembly 5')
+assembly5_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region5] * 10] * 10
+assembly5_lattice.setUniverses(template)
+assembly5_cell.setFill(assembly5_lattice)
 
+# Assembly 6
+assembly6_lattice = Lattice(name='assembly 6')
+assembly6_lattice.setWidth(width_x=1.5, width_y=1.5)
+template = [[region6] * 10] * 10
+assembly6_lattice.setUniverses(template)
+assembly6_cell.setFill(assembly6_lattice)
 
-assembly3 = Lattice(name='assembly 2')
-assembly3.setWidth(width_x=1.5, width_y=1.5)
-template = [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]
+# Full core
+core_lattice = Lattice(name='core')
+core_lattice.setWidth(width_x=15.0, width_y=15.0)
 
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly3.setUniverses(template)
-
-
-assembly4 = Lattice(name='assembly 4')
-assembly4.setWidth(width_x=1.5, width_y=1.5)
-template = [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]]
-
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly4.setUniverses(template)
-
-assembly5 = Lattice(name='assembly 5')
-assembly5.setWidth(width_x=1.5, width_y=1.5)
-template = [[5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]]
-
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly5.setUniverses(template)
-
-
-assembly6 = Lattice(name='assembly 6')
-assembly6.setWidth(width_x=1.5, width_y=1.5)
-template = [[6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6]]
-
-for i in range(10):
-  for j in range(10):
-    template[i][j] = universes[template[i][j]-1]
-assembly6.setUniverses(template)
-
-
-core = Lattice(name='core')
-core.setWidth(width_x=15.0, width_y=15.0)
+universes = {7 : assembly1, 8 : assembly2, 9: assembly3,
+             10 : assembly4, 11 : assembly5, 12 : assembly6}
 template = [[12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12],
             [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12],
             [ 9,  9,  9,  9,  9,  9,  9, 12, 12, 12, 12],
@@ -255,17 +190,9 @@ template = [[12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12],
 
 for i in range(11):
   for j in range(11):
-    template[i][j] = universes[template[i][j]-1]
-core.setUniverses(template)
-
-
-cells[6].setFill(assembly1)
-cells[7].setFill(assembly2)
-cells[8].setFill(assembly3)
-cells[9].setFill(assembly4)
-cells[10].setFill(assembly5)
-cells[11].setFill(assembly6)
-cells[12].setFill(core)
+    template[i][j] = universes[template[i][j]]
+core_lattice.setUniverses(template)
+root_cell.setFill(core_lattice)
 
 
 ###############################################################################
@@ -286,7 +213,7 @@ log.py_printf('NORMAL', 'Creating geometry...')
 
 geometry = Geometry()
 geometry.setCmfd(cmfd)
-geometry.setRootUniverse(root)
+geometry.setRootUniverse(root_universe)
 geometry.initializeFlatSourceRegions()
 
 
@@ -317,12 +244,11 @@ solver.printTimerReport()
 
 log.py_printf('NORMAL', 'Plotting data...')
 
-#plotter.plot_tracks(track_generator)
 #plotter.plot_materials(geometry, gridsize=500)
 #plotter.plot_cells(geometry, gridsize=500)
 #plotter.plot_flat_source_regions(geometry, gridsize=500)
-#plotter.plot_fluxes(geometry, solver, energy_groups=[1,2,3,4,5,6,7])
-#plotter.plot_mesh_fluxes(mesh, energy_groups=[1,2,3,4,5,6,7])
+#plotter.plot_fluxes(geometry, solver, energy_groups=[1,2])
+#plotter.plot_mesh_fluxes(mesh, energy_groups=[1,2])
 #plotter.plot_cmfd_cells(geometry, cmfd, gridsize=500)
 
 log.py_printf('TITLE', 'Finished')
