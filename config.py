dcced3e490f6a7b060bbdb9dc6aa7d7793bed007
Will Boyd <wbinventor@gmail.com>
2015-01-15 08:39:58 -0500

    Added switch to config.py for -py3 SWIG flag only for Python 3

--- config.py
+++ config.py
@@ -269,7 +269,11 @@ class configuration:
   ###########################################################################
 
   # A list of the flags for SWIG
-  swig_flags = ['-c++', '-python', '-keyword', '-py3']
+  swig_flags = ['-c++', '-python', '-keyword']
+
+  # Python 3 only
+  if sys.version_info[0] == 3:
+    swig_flags.append('-py3')
 
 
   #############################################################################
