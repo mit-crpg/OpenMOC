1904bd2e09b48ee21c0a8fef16407b97c58d054d
Will Boyd <wbinventor@gmail.com>
2015-04-05 22:37:17 -0400

    Updated all sample input files to new openmoc.process.plot_spatial_fluxes(...) routine name

--- sample-input/benchmarks/LRA/LRA.py
+++ sample-input/benchmarks/LRA/LRA.py
@@ -237,6 +237,6 @@ log.py_printf('NORMAL', 'Plotting data...')
 #plotter.plot_materials(geometry, gridsize=500)
 #plotter.plot_cells(geometry, gridsize=500)
 #plotter.plot_flat_source_regions(geometry, gridsize=500)
-#plotter.plot_fluxes(geometry, solver, energy_groups=[1,2])
+#plotter.plot_spatial_fluxes(solver, energy_groups=[1,2])
 
 log.py_printf('TITLE', 'Finished')
