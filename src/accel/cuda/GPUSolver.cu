e14face09befe01a12ed0ce341890629a2657afa
Will Boyd <wbinventor@gmail.com>
2015-07-25 21:47:08 -0700

    Made Solver::getFSRSource(...) consistent with GPUSolver

--- src/accel/cuda/GPUSolver.cu
+++ src/accel/cuda/GPUSolver.cu
@@ -181,7 +181,7 @@ __global__ void computeFSRSourcesOnDevice(int* FSR_materials,
       fission_source *= inverse_k_eff;
 
       /* Compute total (scatter+fission+fixed) reduced source */
-      reduced_sources(tid,G) = fixed_sources(tid,G)
+      reduced_sources(tid,G) = fixed_sources(tid,G);
       reduced_sources(tid,G) += scatter_source + fission_source;
       reduced_sources(tid,G) = __fdividef(reduced_sources(tid,G), sigma_t[G]);
     }
