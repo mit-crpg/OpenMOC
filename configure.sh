ba97d63bd97a90a94dde7c9ba10044258523fb3e
Sam Shaner <shaner@mit.edu>
2016-01-22 12:10:40 -0500

    added updated png files

--- configure.sh
+++ configure.sh
@@ -1,10 +1,4 @@
-cd docs/doxygen
-doxygen Doxyfile
-cd ..
-make html
-cd ..
-cp -rf docs/build/html/. .
 git add .
-git add -f _images/* .
-git add -f _images/math/* .
-git add -f doxygen/html/* .
+git add -f _images/*
+git add -f _images/math/*
+git add -f doxygen/html/*
