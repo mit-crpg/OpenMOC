4595c50657d47f77460bf0951db2bd56d3ff5446
geogunow <geogunow@mit.edu>
2016-04-20 23:31:58 -0400

    Added abstracted track traversing routines

---
+++ src/TrackTraversingAlgorithms.cpp
@@ -0,0 +1,127 @@
+#include "TrackTraversingAlgorithms.h"
+#include "CPUSolver.h"
+#include "Quadrature.h"
+
+
+/**
+ * @brief Constructor for SegmentSplitter calls the TraverseTracks
+ *        constructor
+ * @param track_generator The TrackGenerator to pull tracking information from
+ */
+VolumeCalculator::VolumeCalculator(TrackGenerator* track_generator)
+                                  : TraverseTracks(track_generator) {
+}
+
+
+/**
+ * @brief FSR volumes are calculated and saved in the TrackGenerator's FSR
+ *        volumes buffer
+ * @details VolumeKernels are created and used to loop over all segments and
+ *          tally each segments contribution to FSR volumes.
+ */
+void VolumeCalculator::execute() {
+#pragma omp parallel
+  {
+    MOCKernel** kernels = getKernels<VolumeKernel>();
+    loopOverTracks(kernels);
+  }
+}
+
+
+/**
+ * @brief No functionality is applied for each Track during the execution of
+ *        the VolumeCalculator
+ * @param track The current Track
+ * @param segments The segments associated with the Track
+ */
+void VolumeCalculator::onTrack(Track* track, segment* segments) {
+}
+
+
+/**
+ * @brief Constructor for TransportSweep calls the TraverseTracks
+ *        constructor and initializes the associated CPUSolver to NULL
+ * @param track_generator The TrackGenerator to pull tracking information from
+ */
+TransportSweep::TransportSweep(TrackGenerator* track_generator)
+                              : TraverseTracks(track_generator) {
+  _cpu_solver = NULL;
+}
+
+
+/**
+ * @brief MOC equations are applied to every segment in the TrackGenerator
+ * @details onTrack(...) applies the MOC equations to each segment and
+ *          transfers boundary fluxes for the corresponding Track.
+ */
+void TransportSweep::execute() {
+#pragma omp parallel
+  {
+    loopOverTracksByParallelGroup(NULL);
+  }
+}
+
+
+/**
+ * @brief Sets the CPUSolver so that TransportSweep can apply MOC equations
+ * @details This allows TransportSweep to transfer boundary fluxes from the
+ *          CPUSolver and tally scalar fluxes
+ * @param cpu_solver The CPUSolver which applies the MOC equations
+ */
+void TransportSweep::setCPUSolver(CPUSolver* cpu_solver) {
+  _cpu_solver = cpu_solver;
+}
+
+
+/**
+ * @brief Applies the MOC equations the Track and segments
+ * @details The MOC equations are applied to each segment, attenuating the
+ *          Track's angular flux and tallying FSR contributions. Finally,
+ *          Track boundary fluxes are transferred.
+ * @param track The Track for which the angular flux is attenuated and
+ *        transferred
+ * @param segments The segments over which the MOC equations are applied
+ */
+void TransportSweep::onTrack(Track* track, segment* segments) {
+
+  /* Allocate temporary FSR flux locally */
+  int num_groups = _track_generator->getGeometry()->getNumEnergyGroups();
+  FP_PRECISION thread_fsr_flux[num_groups];
+
+  /* Extract Track information */
+  int track_id = track->getUid();
+  int azim_index = track->getAzimAngleIndex();
+  int num_segments = track->getNumSegments();
+  FP_PRECISION* track_flux;
+
+  /* Correct azimuthal index to first octant */
+  Quadrature* quad = _track_generator->getQuadrature();
+
+  /* Get the forward track flux */
+  track_flux = _cpu_solver->getBoundaryFlux(track_id, true);
+
+  /* Loop over each Track segment in forward direction */
+  for (int s=0; s < num_segments; s++) {
+    segment* curr_segment = &segments[s];
+    _cpu_solver->tallyScalarFlux(curr_segment, azim_index, track_flux,
+                                 thread_fsr_flux);
+    _cpu_solver->tallyCurrent(curr_segment, azim_index, track_flux, true);
+  }
+
+  /* Transfer boundary angular flux to outgoing Track */
+  _cpu_solver->transferBoundaryFlux(track_id, azim_index, true, track_flux);
+
+  /* Get the backward track flux */
+  track_flux = _cpu_solver->getBoundaryFlux(track_id, false);
+
+  /* Loop over each Track segment in reverse direction */
+  for (int s=num_segments-1; s >= 0; s--) {
+    segment* curr_segment = &segments[s];
+    _cpu_solver->tallyScalarFlux(curr_segment, azim_index, track_flux,
+                                 thread_fsr_flux);
+    _cpu_solver->tallyCurrent(curr_segment, azim_index, track_flux, false);
+  }
+
+  /* Transfer boundary angular flux to outgoing Track */
+  _cpu_solver->transferBoundaryFlux(track_id, azim_index, false, track_flux);
+}
---
+++ src/TrackTraversingAlgorithms.h
@@ -0,0 +1,73 @@
+/**
+ * @file TrackTraversingAlgorithms.h
+ * @brief Contains classes which extend the TraverseTracks class to apply
+ *        algorithms to Tracks and possibly their segments
+ * @details The classes defined within this file extend the TraverseTracks
+ *          class so that they are capable of using the abstract looping
+ *          defined in TraverseTracks::loopOverTracks(...). Each class
+ *          contains a constructor which pulls data from a provided
+ *          TrackGenerator, an onTrack(...) function which specifies what to do
+ *          on each Track, and an execute() function which applies the
+ *          algorithm. The execute() function should contain a call to
+ *          TraverseTracks::loopOverTracks(...). To specify a behavior to
+ *          be applied once for each segment, kernels should be initialized
+ *          using the TraverseTracks::getKernels<KernelType>() function and
+ *          passed to TraverseTracks::loopOverTracks().
+ * @date February 23, 2016
+ * @author Geoffrey Gunow, MIT, Course 22 (geogunow@mit.edu)
+ */
+
+#ifndef TRACK_TRAVERSING_ALGORITHMS_H_
+#define TRACK_TRAVERSING_ALGORITHMS_H_
+
+#include "TraverseTracks.h"
+
+
+/** Forward declaration of CPUSolver class */
+class CPUSolver;
+
+
+/**
+ * @class VolumeCalculator TrackTraversingAlgorithms.h
+ *        "src/TrackTraversingAlgorithms.h"
+ * @brief A class used to calculate FSR volumes
+ * @details A VolumeCalculator imports a buffer to store FSR volumes from the
+ *          provided TrackGenerator and the allocates VolumeKernels to
+ *          calculate and update the volumes in each FSR, implicitly writing
+ *          the calculated volumes back to the TrackGenerator.
+ */
+class VolumeCalculator: public TraverseTracks {
+
+public:
+
+  VolumeCalculator(TrackGenerator* track_generator);
+  void execute();
+  void onTrack(Track* track, segment* segments);
+};
+
+
+/**
+ * @class TransportSweep TrackTraversingAlgorithms.h
+ *        "src/TrackTraversingAlgorithms.h"
+ * @brief A class used to apply the MOC transport equations to all segments
+ * @details TransportSweep imports data from the provided TrackGenerator and
+ *          using a provided CPUSolver, it applies the MOC equations to each
+ *          segment, tallying the contributions to each FSR. At the end of each
+ *          Track, boundary fluxes are exchanged based on boundary conditions.
+ */
+class TransportSweep: public TraverseTracks {
+
+private:
+
+  CPUSolver* _cpu_solver;
+
+public:
+
+  TransportSweep(TrackGenerator* track_generator);
+  void setCPUSolver(CPUSolver* cpu_solver);
+  void execute();
+  void onTrack(Track* track, segment* segments);
+};
+
+
+#endif
