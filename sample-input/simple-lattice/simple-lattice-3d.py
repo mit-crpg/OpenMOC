17b406b2e2507e8f006bb7a9c46a3649febcd97a
geogunow <geogunow@mit.edu>
2016-05-17 17:01:35 -0400

    Fixed inputs

--- sample-input/simple-lattice/simple-lattice-3d.py
+++ sample-input/simple-lattice/simple-lattice-3d.py
@@ -29,6 +29,7 @@ track_generator = openmoc.TrackGenerator3D(geometry, num_azim, num_polar,
                                            azim_spacing, polar_spacing)
 track_generator.setNumThreads(num_threads)
 track_generator.setTrackGenerationMethod(openmoc.MODULAR_RAY_TRACING)
+track_generator.setSegmentFormation(openmoc.OTF_STACKS)
 track_generator.generateTracks()
 
 ###############################################################################
