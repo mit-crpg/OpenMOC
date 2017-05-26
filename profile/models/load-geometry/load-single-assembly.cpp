085e7dfc3e7dae1046d4f92c9c93ee34f9030515
Geoff Gunow <geogunow@cetuslac1.mcp.alcf.anl.gov>
2017-05-26 19:54:26 +0000

    Updated single assembly to full height

--- profile/models/load-geometry/load-single-assembly.cpp
+++ profile/models/load-geometry/load-single-assembly.cpp
@@ -20,8 +20,8 @@ int main(int argc,  char* argv[]) {
   //std::string file = "single-assembly-5G-adjust.geo";
   //std::string file = "single-assembly-5G-adjust-no-neg.geo";
   //std::string file = "single-no-TC.geo";
-  //std::string file = "v1-single-assembly.geo";
-  std::string file = "v1-single-assembly-2D.geo";
+  std::string file = "v1-single-assembly.geo";
+  //std::string file = "v1-single-assembly-2D.geo";
 
   /* Define simulation parameters */
   #ifdef OPENMP
@@ -41,7 +41,7 @@ int main(int argc,  char* argv[]) {
   /* Create CMFD lattice */
   Cmfd cmfd;
   cmfd.useAxialInterpolation(true);
-  cmfd.setLatticeStructure(17, 17, 5); //FIXME 200
+  cmfd.setLatticeStructure(17, 17, 200); //FIXME 5
   cmfd.setKNearest(1);
   std::vector<std::vector<int> > cmfd_group_structure =
       get_group_structure(70, 8);
@@ -119,7 +119,7 @@ int main(int argc,  char* argv[]) {
   log_printf(NORMAL, "Pitch = %8.6e", geometry.getMaxX() - geometry.getMinX());
   log_printf(NORMAL, "Height = %8.6e", geometry.getMaxZ() - geometry.getMinZ());
 #ifdef MPIx
-  geometry.setDomainDecomposition(17, 17, 1, MPI_COMM_WORLD); //FIXME 17x17xN
+  geometry.setDomainDecomposition(1, 1, 40, MPI_COMM_WORLD); //FIXME 17x17xN
 #endif
   //geometry.setNumDomainModules(17, 17, 1);
   geometry.setOverlaidMesh(2.0);
@@ -132,19 +132,19 @@ int main(int argc,  char* argv[]) {
   track_generator.setTrackGenerationMethod(MODULAR_RAY_TRACING);
   track_generator.setNumThreads(num_threads);
   track_generator.setSegmentFormation(OTF_STACKS);
-/* FIXME
   double z_arr[] = {20., 34., 36., 38., 40., 98., 104., 150., 156., 202., 208.,
                     254., 260., 306., 312., 360., 366., 400., 402., 412., 414.,
                     418., 420.};
-                    */
+/* FIXME
   double z_arr[] = {120., 130.};
+                    */
   std::vector<double> segmentation_zones(z_arr, z_arr + sizeof(z_arr)
                                          / sizeof(double));
   track_generator.setSegmentationZones(segmentation_zones);
   track_generator.generateTracks();
 
   /* Run simulation */
-  CPUSolver solver(&track_generator); //FIXME LS / FS
+  CPULSSolver solver(&track_generator); //FIXME LS / FS
   solver.setNumThreads(num_threads);
   solver.setVerboseIterationReport();
   solver.setConvergenceThreshold(tolerance);
@@ -154,7 +154,7 @@ int main(int argc,  char* argv[]) {
 
   Lattice mesh_lattice;
   Mesh mesh(&solver);
-  mesh.createLattice(17, 17, 1); //FIXME 200
+  mesh.createLattice(17, 17, 200); //FIXME 1
   Vector3D rx_rates = mesh.getFormattedReactionRates(FISSION_RX);
 
   int my_rank = 0;
