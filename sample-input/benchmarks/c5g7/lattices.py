7b326349ea2abed23a4352824a88c31b7f35d936
wuwenbin2006 <wuwenbin2006@gmail.com>
2018-10-25 22:30:00 -0400

    Add non-uniform water gapped C5G7 RoddedB, with gap=0.10

--- sample-input/benchmarks/c5g7/lattices.py
+++ sample-input/benchmarks/c5g7/lattices.py
@@ -1,5 +1,7 @@
 import openmoc
+import numpy as np
 from universes import universes, cells, surfaces
+from surfaces import gap
 
 ###############################################################################
 #########################   Set Simulation Param   ############################
@@ -26,6 +28,18 @@ lattices['UO2 Rodded Assembly']         = openmoc.Lattice()
 lattices['MOX Unrodded Assembly']       = openmoc.Lattice()
 lattices['MOX Rodded Assembly']         = openmoc.Lattice()
 lattices['Root']                        = openmoc.Lattice()
+
+#lattices['Gap Refined Reflector Mesh']      = openmoc.Lattice()
+#lattices['Gap Reflector Unrodded Assembly'] = openmoc.Lattice()
+lattices['Gap Reflector Rodded Assembly']   = openmoc.Lattice()
+lattices['Gap Reflector Right Assembly']    = openmoc.Lattice()
+lattices['Gap Reflector Bottom Assembly']   = openmoc.Lattice()
+lattices['Gap Reflector Corner Assembly']   = openmoc.Lattice()
+#lattices['Gap Reflector Assembly']          = openmoc.Lattice()
+lattices['Gap UO2 Unrodded Assembly']       = openmoc.Lattice()
+lattices['Gap UO2 Rodded Assembly']         = openmoc.Lattice()
+lattices['Gap MOX Unrodded Assembly']       = openmoc.Lattice()
+lattices['Gap MOX Rodded Assembly']         = openmoc.Lattice()
 
 # Abbreviate universes that will fill lattices
 u = universes['UO2']
@@ -203,6 +217,184 @@ lattices['Reflector Assembly'].setWidth(width_x=1.26, width_y=1.26, width_z=100.
 template = [[[a] * 17] * 17]
 lattices['Reflector Assembly'].setUniverses(template)
 
+
+
+row = np.array([[r]*17])
+col = np.array([[r]*19]).reshape(-1, 1)
+width = [
+					[gap] + [1.26]*17 + [gap],
+					[gap] + [1.26]*17 + [gap],
+					[100]
+					]
+
+# Gap UO2 unrodded 17 x 17 assemblies	
+lattices['Gap UO2 Unrodded Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, g, u, u, g, u, u, g, u, u, u, u, u],
+             [u, u, u, g, u, u, u, u, u, u, u, u, u, g, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, g, u, u, g, u, u, g, u, u, g, u, u, g, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, g, u, u, g, u, u, f, u, u, g, u, u, g, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, g, u, u, g, u, u, g, u, u, g, u, u, g, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, g, u, u, u, u, u, u, u, u, u, g, u, u, u],
+             [u, u, u, u, u, g, u, u, g, u, u, g, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u]]]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap UO2 Unrodded Assembly'].setUniverses(template)
+
+# Gap UO2 rodded 17 x 17 assemblies
+lattices['Gap UO2 Rodded Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, c, u, u, c, u, u, c, u, u, u, u, u],
+             [u, u, u, c, u, u, u, u, u, u, u, u, u, c, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, c, u, u, c, u, u, c, u, u, c, u, u, c, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, c, u, u, c, u, u, f, u, u, c, u, u, c, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, c, u, u, c, u, u, c, u, u, c, u, u, c, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, c, u, u, u, u, u, u, u, u, u, c, u, u, u],
+             [u, u, u, u, u, c, u, u, c, u, u, c, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u],
+             [u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u, u]]]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap UO2 Rodded Assembly'].setUniverses(template)
+
+# Gap MOX unrodded 17 x 17 assemblies
+lattices['Gap MOX Unrodded Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m],
+             [m, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, m],
+             [m, o, o, o, o, g, o, o, g, o, o, g, o, o, o, o, m],
+             [m, o, o, g, o, x, x, x, x, x, x, x, o, g, o, o, m],
+             [m, o, o, o, x, x, x, x, x, x, x, x, x, o, o, o, m],
+             [m, o, g, x, x, g, x, x, g, x, x, g, x, x, g, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, g, x, x, g, x, x, f, x, x, g, x, x, g, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, g, x, x, g, x, x, g, x, x, g, x, x, g, o, m],
+             [m, o, o, o, x, x, x, x, x, x, x, x, x, o, o, o, m],
+             [m, o, o, g, o, x, x, x, x, x, x, x, o, g, o, o, m],
+             [m, o, o, o, o, g, o, o, g, o, o, g, o, o, o, o, m],
+             [m, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, m],
+             [m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m]]]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap MOX Unrodded Assembly'].setUniverses(template)
+
+# Gap MOX rodded 17 x 17 assemblies
+lattices['Gap MOX Rodded Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m],
+             [m, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, m],
+             [m, o, o, o, o, c, o, o, c, o, o, c, o, o, o, o, m],
+             [m, o, o, c, o, x, x, x, x, x, x, x, o, c, o, o, m],
+             [m, o, o, o, x, x, x, x, x, x, x, x, x, o, o, o, m],
+             [m, o, c, x, x, c, x, x, c, x, x, c, x, x, c, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, c, x, x, c, x, x, f, x, x, c, x, x, c, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, o, x, x, x, x, x, x, x, x, x, x, x, o, o, m],
+             [m, o, c, x, x, c, x, x, c, x, x, c, x, x, c, o, m],
+             [m, o, o, o, x, x, x, x, x, x, x, x, x, o, o, o, m],
+             [m, o, o, c, o, x, x, x, x, x, x, x, o, c, o, o, m],
+             [m, o, o, o, o, c, o, o, c, o, o, c, o, o, o, o, m],
+             [m, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, m],
+             [m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m]]]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap MOX Rodded Assembly'].setUniverses(template)
+
+# Gap Reflector rodded 17 x 17 assemblies
+lattices['Gap Reflector Rodded Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, p, p, c, p, p, c, p, p, c, p, p, p, p, p],
+             [p, p, p, c, p, p, p, p, p, p, p, p, p, c, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, c, p, p, c, p, p, c, p, p, c, p, p, c, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, c, p, p, c, p, p, f, p, p, c, p, p, c, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, c, p, p, c, p, p, c, p, p, c, p, p, c, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, c, p, p, p, p, p, p, p, p, p, c, p, p, p],
+             [p, p, p, p, p, c, p, p, c, p, p, c, p, p, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p],
+             [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p]]]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap Reflector Rodded Assembly'].setUniverses(template)
+
+# Gap Reflector right 17 x 17 assemblies
+lattices['Gap Reflector Right Assembly'].setWidths(width[0], width[1], width[2])
+template = [[[a] * 11 + [r] * 6] * 17]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap Reflector Right Assembly'].setUniverses(template)
+
+# Gap Reflector bottom 17 x 17 assemblies
+lattices['Gap Reflector Bottom Assembly'].setWidths(width[0], width[1], width[2])
+template = [[a] * 17] * 11
+template += [[r] * 17] * 6
+template = [template]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap Reflector Bottom Assembly'].setUniverses(template)
+
+# Gap Reflector corner 17 x 17 assemblies
+lattices['Gap Reflector Corner Assembly'].setWidths(width[0], width[1], width[2])
+template = [[a] * 11 + [r] * 6] * 11
+template += [[r] * 17] * 6
+template = [template]
+template = np.array(template)
+template = np.concatenate((row,template[0],row),axis=0)
+template = np.concatenate((col,template,col),axis=1)
+template = template[np.newaxis,:]
+template = template.tolist()
+lattices['Gap Reflector Corner Assembly'].setUniverses(template)
+
+
+
+
+
 # Fill cells with lattices
 cells['Refined Reflector Mesh']     .setFill(lattices['Refined Reflector Mesh'])
 cells['UO2 Unrodded Assembly']      .setFill(lattices['UO2 Unrodded Assembly'])
@@ -216,3 +408,16 @@ cells['Reflector Bottom Assembly']  .setFill(lattices['Reflector Bottom Assembly
 cells['Reflector Corner Assembly']  .setFill(lattices['Reflector Corner Assembly'])
 cells['Reflector Assembly']         .setFill(lattices['Reflector Assembly'])
 cells['Root']                       .setFill(lattices['Root'])
+cells['Gap Root']                   .setFill(lattices['Root'])
+
+
+cells['Gap UO2 Unrodded Assembly']      .setFill(lattices['Gap UO2 Unrodded Assembly'])
+cells['Gap UO2 Rodded Assembly']        .setFill(lattices['Gap UO2 Rodded Assembly'])
+cells['Gap MOX Unrodded Assembly']      .setFill(lattices['Gap MOX Unrodded Assembly'])
+cells['Gap MOX Rodded Assembly']        .setFill(lattices['Gap MOX Rodded Assembly'])
+#cells['Gap Reflector Unrodded Assembly'].setFill(lattices['Gap Reflector Unrodded Assembly'])
+cells['Gap Reflector Rodded Assembly']  .setFill(lattices['Gap Reflector Rodded Assembly'])
+cells['Gap Reflector Right Assembly']   .setFill(lattices['Gap Reflector Right Assembly'])
+cells['Gap Reflector Bottom Assembly']  .setFill(lattices['Gap Reflector Bottom Assembly'])
+cells['Gap Reflector Corner Assembly']  .setFill(lattices['Gap Reflector Corner Assembly'])
+#cells['Gap Reflector Assembly']         .setFill(lattices['Gap Reflector Assembly'])
--- sample-input/benchmarks/c5g7/surfaces.py
+++ sample-input/benchmarks/c5g7/surfaces.py
@@ -4,6 +4,7 @@ import openmoc
 ###########################   Creating Surfaces   #############################
 ###############################################################################
 
+gap = 0.5
 surfaces = {}
 
 # Instantiate surfaces
@@ -16,6 +17,10 @@ surfaces['Root Small z-max'] = openmoc.ZPlane(z= 32.13, name='Root Small z-max')
 surfaces['Root Big z-min']   = openmoc.ZPlane(z=-107.1, name='Root Big z-min')
 surfaces['Root Big z-max']   = openmoc.ZPlane(z= 107.1, name='Root Big z-max')
 surfaces['Fuel Cylinder']    = openmoc.ZCylinder(x=0.0, y=0.0, radius=0.54, name='Fuel Cylinder')
+surfaces['Gap Root x-min']       = openmoc.XPlane(x=-32.13 - 3*gap, name='Gap Root x-min')
+surfaces['Gap Root x-max']       = openmoc.XPlane(x= 32.13 + 3*gap, name='Gap Root x-max')
+surfaces['Gap Root y-min']       = openmoc.YPlane(y=-32.13 - 3*gap, name='Gap Root y-min')
+surfaces['Gap Root y-max']       = openmoc.YPlane(y= 32.13 + 3*gap, name='Gap Root y-max')
 
 surfaces['Root x-min'].setBoundaryType(openmoc.REFLECTIVE)
 surfaces['Root x-max'].setBoundaryType(openmoc.VACUUM)
@@ -25,3 +30,7 @@ surfaces['Root Small z-min'].setBoundaryType(openmoc.REFLECTIVE)
 surfaces['Root Small z-max'].setBoundaryType(openmoc.VACUUM)
 surfaces['Root Big z-min'].setBoundaryType(openmoc.REFLECTIVE)
 surfaces['Root Big z-max'].setBoundaryType(openmoc.VACUUM)
+surfaces['Gap Root x-min'].setBoundaryType(openmoc.REFLECTIVE)
+surfaces['Gap Root x-max'].setBoundaryType(openmoc.VACUUM)
+surfaces['Gap Root y-min'].setBoundaryType(openmoc.VACUUM)
+surfaces['Gap Root y-max'].setBoundaryType(openmoc.REFLECTIVE)
