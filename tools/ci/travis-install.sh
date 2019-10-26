caac771bad4a6a88d19752abd41c68b0610c33a0
guillaume <g_giud@mit.edu>
2019-10-26 13:04:16 -0400

    Add superhomogeneization module test

--- tools/ci/travis-install.sh
+++ tools/ci/travis-install.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -ex
 
+# Handle Python third-party packages
+if [ -f $HOME/miniconda ]; then
+    echo "Using cached Miniconda"
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+  # conda update -q --force conda
+    conda info -a
+    conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy h5py pandas matplotlib pillow
+    source activate test-environment
+    sudo apt-get install swig
+  # For coverage testing of C++ and Python source files
+    pip install coverage
+    pip install cpp-coveralls
+    pip install coveralls
+fi
+
+# Install openmc for Superhomogeneization test
+if [-f "openmc" ]; then
+    echo "Using cached OpenMC"
+else
+    git clone --depth=50 --branch=master https://github.com/openmc-dev/openmc.git openmc
+fi 
+
 # Install mpi4py and MPI-wrapped compilers for MPI configurations
 if [[ $MPI == 'y' ]]; then
     $HOME/miniconda/bin/conda config --add channels conda-forge
