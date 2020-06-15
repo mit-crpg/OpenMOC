44358774b862b0e07377ab22bceb0b80bd384211
Giud <g_giud@mit.edu>
2020-06-14 21:01:45 -0400

    Add 3D plot tests for tracks and segments

---
+++ tests/test_plot_3D_tracks/test_plot_3D_tracks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+sys.path.insert(0, os.pardir)
+sys.path.insert(0, os.path.join(os.pardir, 'openmoc'))
+from testing_harness import PlottingTestHarness
+from input_set import AxialExtendedInput
+from openmoc.plotter import plot_tracks
+import openmoc
+
+class PlotTracksTestHarness(PlottingTestHarness):
+    """Test track plotting with a 4x4 lattice."""
+
+    def __init__(self):
+        super(PlotTracksTestHarness, self).__init__()
+        self.input_set = AxialExtendedInput()
+        self.num_polar = 4
+        self.azim_spacing = 0.24
+        self.z_spacing = 1.25
+        self.max_iters = 1
+
+    def _create_trackgenerator(self):
+        """Instantiate a TrackGenerator."""
+        geometry = self.input_set.geometry
+        geometry.initializeFlatSourceRegions()
+        self.track_generator = \
+            openmoc.TrackGenerator3D(geometry, self.num_azim, self.num_polar,
+                                     self.azim_spacing, self.z_spacing)
+        self.track_generator.setSegmentFormation(openmoc.OTF_STACKS)
+
+    def _run_openmoc(self):
+        """Plot the tracks."""
+
+        # Run an eigenvalue calculation to setup track generator
+        super(PlotTracksTestHarness, self)._run_openmoc()
+
+        # Create Matplotlib Figures for the tracks
+        self.figures = [plot_tracks(self.track_generator, get_figure=True),
+             plot_tracks(self.track_generator, get_figure=True, plot_3D=True)]
+
+
+if __name__ == '__main__':
+    harness = PlotTracksTestHarness()
+    harness.main()
